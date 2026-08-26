1d4406f77f9be0f3983ae5021927d319f7055e0c
Godecode62 <godecode62@gmail.com>
2026-08-26 16:48:16 +0000

    Restaure la disposition initiale de la navbar

--- ecole_moderne/test_navbar_responsive.py
+++ ecole_moderne/test_navbar_responsive.py
@@ -6,13 +6,17 @@ class NavbarResponsiveTests(SimpleTestCase):
     def setUp(self):
         self.response = self.client.get('/rapport-scolaire/')
 
-    def test_navigation_se_replie_avant_1400_pixels(self):
+    def test_navigation_conserve_la_disposition_initiale(self):
         self.assertEqual(self.response.status_code, 200)
         self.assertContains(
             self.response,
-            'navbar navbar-expand-xxl navbar-dark bg-primary fixed-top site-navbar',
+            'navbar navbar-expand-lg navbar-dark bg-primary fixed-top site-navbar',
         )
-        self.assertContains(self.response, '@media (max-width: 1399.98px)')
+        self.assertContains(self.response, '@media (max-width: 991.98px)')
+        self.assertContains(
+            self.response,
+            '@media (min-width: 992px) and (max-width: 1399.98px)',
+        )
 
     def test_bouton_mobile_est_accessible_et_libelles_sans_retour(self):
         self.assertContains(self.response, 'aria-controls="navbarNav"')
