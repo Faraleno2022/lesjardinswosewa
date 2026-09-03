3f5cb63978f400d7a64a37d13bc712f90cddb114
Godecode62 <godecode62@gmail.com>
2026-09-03 09:57:57 +0000

    Adapte la navbar aux ordinateurs et tablettes

--- ecole_moderne/test_navbar_responsive.py
+++ ecole_moderne/test_navbar_responsive.py
@@ -15,7 +15,7 @@ class NavbarResponsiveTests(SimpleTestCase):
         self.assertContains(self.response, '@media (max-width: 991.98px)')
         self.assertContains(
             self.response,
-            '@media (min-width: 992px) and (max-width: 1399.98px)',
+            '@media (max-width: 1399.98px)',
         )
 
     def test_bouton_mobile_est_accessible_et_libelles_sans_retour(self):
@@ -50,3 +50,32 @@ class NavbarResponsiveTests(SimpleTestCase):
         self.assertContains(self.response, 'min-width: 44px;')
         self.assertContains(self.response, 'touch-action: manipulation;')
         self.assertContains(self.response, 'flex-basis: 100%;')
+
+    def test_ordinateur_conserve_tous_les_boutons_sur_une_ligne(self):
+        self.assertContains(self.response, "'nav-device-desktop'")
+        self.assertContains(
+            self.response,
+            'html.nav-device-desktop .site-navbar .navbar-toggler',
+        )
+        self.assertContains(self.response, 'display: none !important;')
+        self.assertContains(self.response, 'flex-direction: row;')
+        self.assertContains(self.response, 'overflow-x: auto;')
+
+    def test_ios_android_et_tablettes_utilisent_le_menu_hamburger(self):
+        self.assertContains(self.response, "'nav-device-mobile'")
+        self.assertContains(
+            self.response,
+            '/Android|iPhone|iPad|iPod|Mobile/i.test(agent)',
+        )
+        self.assertContains(
+            self.response,
+            "window.matchMedia('(hover: none) and (pointer: coarse)').matches",
+        )
+        self.assertContains(
+            self.response,
+            'html.nav-device-mobile .navbar-expand-lg .navbar-toggler',
+        )
+        self.assertContains(
+            self.response,
+            'html.nav-device-mobile .navbar-expand-lg .navbar-collapse.collapse:not(.show)',
+        )
